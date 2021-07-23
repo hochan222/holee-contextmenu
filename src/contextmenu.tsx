5b1c23d5d704be25de798c229b62a6f6e6990ed9
hochan222 <hochan049@gmail.com>
2021-07-23 18:43:51 +0900

    contextmenu duplicate bug (#16)

--- src/contextmenu.tsx
+++ src/contextmenu.tsx
@@ -31,10 +31,10 @@ const useContextMenu = (outerRef: React.RefObject<HTMLDivElement>) => {
       setXPos(`${event.pageX}px`);
       setYPos(`${event.pageY}px`);
       if (
-        outerRef.current!.getBoundingClientRect().top <= event.clientY &&
-        outerRef.current!.getBoundingClientRect().bottom >= event.clientY &&
-        outerRef.current!.getBoundingClientRect().left <= event.clientX &&
-        outerRef.current!.getBoundingClientRect().right >= event.clientX
+        outerRef.current!.getBoundingClientRect().top < event.clientY &&
+        outerRef.current!.getBoundingClientRect().bottom > event.clientY &&
+        outerRef.current!.getBoundingClientRect().left < event.clientX &&
+        outerRef.current!.getBoundingClientRect().right > event.clientX
       ) {
         event.preventDefault();
         showMenu(true);
